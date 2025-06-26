8469286aface0cefeede7520a57e93e2c2bd632e
Racheloviedo <00033125@uca.edu.sv>
2025-06-26 15:10:42 -0600

    actualizando wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1,5 +1,21 @@
 #include "wallet.h"
-namespace walletCommands{
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include <string> 
+
+//Wallet: billetera personal 
+//finalWallet: representa el resultante que posee el usuario 
+//reultWallet:  representa el resultante que posee el usuario
+
+struct players {
+    string userName;
+    float wallet;
+    string race;
+};
+
+
+    //funcion para sumar dinero ganado a billetera
     float winnerResult(float personalWallet, float winnBet){
         float resultWallet;
         resultWallet = personalWallet + winnBet;
@@ -10,6 +26,7 @@ namespace walletCommands{
 
     }
 
+    //funcion para restar dinero a la billetera
     float loserResult(float personalWallet, float winnBet){
         float minusWallet;
         minusWallet = personalWallet - winnBet;
@@ -19,4 +36,36 @@ namespace walletCommands{
         return minusWallet;
 
     }
-}
+
+    //funcion para crear el historial de la billetera
+    void registerChange(const players& p){
+        ofstream archivo("historial_wallet.txt", ios::app); //ios::app para agregar y no sobreescribir
+        if (archivo.is_open()) {
+            archivo << "------Registro de billetera------\n" << endl;
+            archivo << p.userName << endl;
+            archivo << p.wallet << endl;
+            archivo << p.race << endl;
+            archivo << "---------------------------------\n" << endl;
+            archivo.close();
+    } else {
+        cout << "No se pudo guardar historial.\n";
+    }
+    }
+
+
+    //funcion para mostrar el historial de la billetera
+    void showHistorial(){
+        ifstream archivo("historial_wallet.txt");
+        if (archivo.is_open()) {
+            string line;
+            cout << "\n=====Historial de billetera=====\n"<< endl;
+            while(getline(archivo, line)){
+                cout << line << endl;
+            }
+            archivo.close();
+        } else {
+        cout << "Error: no se pudo abrir el historial.\n";
+    }
+    } 
+
+
--- src/wallet.h
+++ src/wallet.h
@@ -1,10 +1,21 @@
 //wallet.h
+#include <iostream>
+#include <fstream>
+using namespace std;
 #ifndef WALLET_H
 #define WALLET_H
-namespace walletCommands{
+
+struct players {
+    string userName;
+    float wallet;
+    char race[50];
+};
+
     float winnerResult(float personalWallet, float winnBet);
     float loserResult (float personalWallet, float winnBet);
-}
+    void registerChange(const players& p);
+    void showHistorial();
+
 
 #endif 
 
