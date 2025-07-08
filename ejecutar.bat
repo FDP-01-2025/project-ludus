d2a8a0668fc0e29a3e9db09443a6ecbc9822b5ed
EnmanuelPalma <00208225@uca.edu.sv>
2025-07-08 00:05:16 -0600

    formula1...

--- ejecutar.bat
+++ ejecutar.bat
@@ -1,12 +1,24 @@
+
 @echo off
+cd /d "%~dp0"
 echo Compilando proyecto F1...
-g++ -o main.exe src/main.cpp src/header.cpp
-if %errorlevel% == 0 (
-    echo Compilacion exitosa!
-    echo Ejecutando juego...
-    main.exe
+echo Directorio actual: %cd%
+if exist "src\main.cpp" (
+    echo Archivos encontrados, compilando...
+    g++ -o main.exe src\main.cpp src\header.cpp -std=c++11 -static-libgcc -static-libstdc++
+    if %errorlevel% == 0 (
+        echo Compilacion exitosa!
+        echo Ejecutando juego...
+        main.exe
+    ) else (
+        echo Error en la compilacion!
+        echo Presiona cualquier tecla para ver mas detalles...
+        pause
+    )
 ) else (
-    echo Error en la compilacion!
+    echo Error: No se encontraron los archivos fuente!
+    echo Buscando en: %cd%\src\
+    dir src\
     pause
 )
 
